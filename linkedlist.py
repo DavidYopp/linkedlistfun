a166a3ac08540ffeb3d3bbe7c0b5af8c7b75f080
Gdraven99 <DavidJYopp@gmail.com>
2021-06-01 10:42:52 -0400

    committing the linked list py file

---
+++ linkedlist.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__ (self, dataval=None):
+        self.dataval = dataval
+        self.nextnode = None
+
+class LinkedList:
+    def __init__ (self):
+        self.headval = None
+
+    def printlist(self):
+        printval = self.headval
+        while printval is not None:
+            print(printval.dataval)
+            printval = printval.nextnode
+
+    def insertbeginning(self, data):
+        newnode = Node(data)
+
+        newnode.nextnode = self.headval
+        self.headval = newnode
+
+
+linkedlist = LinkedList()
+linkedlist.headval = Node("Mon")
+node2 = Node("Tue")
+node3 = Node("Wed")
+
+linkedlist.headval.nextnode = node2
+node2.nextnode = node3
+
+
+linkedlist.insertbeginning("Sun")
+
+linkedlist.printlist()
